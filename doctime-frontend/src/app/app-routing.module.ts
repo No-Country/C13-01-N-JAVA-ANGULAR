a5db4fdcea5e6f083766eeacf73f72fc871d4bc0
Ruben Mantilla <-h>
2023-08-23 22:09:55 -0500

    feat: create patient profile layout

--- doctime-frontend/src/app/app-routing.module.ts
+++ doctime-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./pages/patient-profile/patient-profile.module').then(
+        (m) => m.PatientProfileModule
+      ),
+  },
+  { path: '**', redirectTo: 'profile' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ doctime-frontend/src/app/pages/patient-profile/patient-profile.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientProfileRoutingModule } from './patient-profile-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PatientProfileRoutingModule],
+})
+export class PatientProfileModule {}
