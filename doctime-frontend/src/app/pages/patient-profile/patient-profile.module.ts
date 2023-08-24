d31a033ae063fb415ad1353ba81833dfa406cece
Ruben Mantilla <-h>
2023-08-24 16:21:07 -0500

    feat: create appointment component & content

--- doctime-frontend/src/app/pages/patient-profile/patient-profile.module.ts
+++ doctime-frontend/src/app/pages/patient-profile/patient-profile.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PatientProfileRoutingModule } from './patient-profile-routing.module';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppointmentsComponent],
   imports: [CommonModule, PatientProfileRoutingModule],
 })
 export class PatientProfileModule {}
