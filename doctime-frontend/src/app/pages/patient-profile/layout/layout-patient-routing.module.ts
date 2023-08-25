5ef5b3e7d14afb2bdf566333266a725b74b8f6a3
Raul Quispe Diaz <66975940+RaulQD@users.noreply.github.com>
2023-08-24 23:04:18 -0500

    feat: profile page created

--- doctime-frontend/src/app/pages/patient-profile/layout/layout-patient-routing.module.ts
+++ doctime-frontend/src/app/pages/patient-profile/layout/layout-patient-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPatientComponent } from './layout-patient.component';
 import { AppointmentsComponent } from '../components/appointments/appointments.component';
+import { SettingsComponent } from '../components/settings/settings.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     component: LayoutPatientComponent,
     children: [
       { path: '', redirectTo: 'appointment', pathMatch: 'full' },
+      { path: 'settings', component: SettingsComponent },
       { path: 'appointment', component: AppointmentsComponent },
       // TODO: add routes here
       { path: '**', redirectTo: 'appointment' },
